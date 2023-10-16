00fbe460942262798f0a5b623f2256abf22cda38
DanSaurio <a22311168@uthermosillo.edu.mx>
2023-10-16 15:54:41 -0700

    Layout Terminado

--- app/src/main/java/com/narnia/dt/convertidortemperatura/MainActivity.kt
+++ app/src/main/java/com/narnia/dt/convertidortemperatura/MainActivity.kt
@@ -2,10 +2,30 @@ package com.narnia.dt.convertidortemperatura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinnerTemp)
+        val RES = findViewById<TextView>(R.id.textVResultado)
+        val temp = findViewById<EditText>(R.id.editTextNTemp)
+        val buttonConvertir = findViewById<Button>(R.id.buttonConvertir)
+
+        buttonConvertir.setOnClickListener{
+            Toast.makeText(this, ""+ temp.text , Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    public fun converter(){
+
+        //TODO Add when statement to process conversions.
     }
 }
---
+++ app/src/main/java/com/narnia/dt/convertidortemperatura/models/Temperature.kt
@@ -0,0 +1,7 @@
+package com.narnia.dt.convertidortemperatura.models
+
+data class Temperature(
+    val temp: Double,
+    val origin: Char,
+    val destiny: Char
+)
