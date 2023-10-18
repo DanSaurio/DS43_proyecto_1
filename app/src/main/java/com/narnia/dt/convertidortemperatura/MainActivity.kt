4042f4b7c683a639ace1ce3459c2b00ff96aca4c
DanSaurio <a22311168@uthermosillo.edu.mx>
2023-10-17 18:59:27 -0700

    Seleccion de spinner y codigo Main

--- app/src/main/java/com/narnia/dt/convertidortemperatura/MainActivity.kt
+++ app/src/main/java/com/narnia/dt/convertidortemperatura/MainActivity.kt
@@ -2,6 +2,9 @@ package com.narnia.dt.convertidortemperatura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
@@ -9,6 +12,8 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var conversion : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,14 +23,69 @@ class MainActivity : AppCompatActivity() {
         val temp = findViewById<EditText>(R.id.editTextNTemp)
         val buttonConvertir = findViewById<Button>(R.id.buttonConvertir)
 
+        val options = resources.getStringArray(R.array.convert_options)
+
+        if (spinner != null ){
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?, positions: Int, id: Long){
+                    Toast.makeText(this@MainActivity, "Opcion Seleccionada", Toast.LENGTH_SHORT).show()
+
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+
+        }
+
+
+
         buttonConvertir.setOnClickListener{
+            converter(conversion)
             Toast.makeText(this, ""+ temp.text , Toast.LENGTH_SHORT).show()
         }
 
     }
 
-    public fun converter(){
+     fun converter(convert: Int){
 
+        when(convert) {
+            0 ->{
+                Toast.makeText(this, "Opcion no Valida", Toast.LENGTH_SHORT).show()
+            }
+            1 ->{
+                //°C to °F
+            }
+            2 ->{
+                //°C to °K
+
+            }
+            3 ->{
+                //°F to °C
+
+            }
+            4 ->{
+                //°F to °K
+
+            }
+            5 ->{
+                //°K to °C
+            }
+            6 ->{
+                //°K to °K
+
+            }
+
+        }
         //TODO Add when statement to process conversions.
     }
 }
